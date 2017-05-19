80ec28a412317bd1a2a56f69a9a40af49422f190
Eugene Bekker <ebekker@powervision.com>
2017-05-19 16:50:27 -0400

    Adding cmdlet to create new auth tokens

---
+++ src/main/Zyborg.Vault.POSH/NewAuth.cs
@@ -0,0 +1,86 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using VaultSharp.Backends.Authentication.Models;
+using VaultSharp.Backends.System.Models;
+
+namespace Zyborg.Vault.POSH
+{
+	[Cmdlet(VerbsCommon.New, "Auth", DefaultParameterSetName = DefaultParamSet)]
+	[OutputType(typeof(AuthorizationInfo))]
+	public class NewAuth : VaultBaseCmdlet
+	{
+		public const string SaveAsParamSet = "SaveAs";
+
+		[Parameter(Mandatory = false)]
+		public string AuthMethod
+		{ get; set; } = "token";
+
+		[Parameter(Mandatory = false)]
+		public string MountName
+		{ get; set; }
+
+		[Parameter(Mandatory = false)]
+		public string PathData
+		{ get; set; }
+
+		[Parameter(Mandatory = false)]
+		public Hashtable AuthData
+		{ get; set; }
+
+		[Parameter(Mandatory = false)]
+		public SwitchParameter NoVerify
+		{ get; set; }
+
+		[Parameter(Mandatory = false, ParameterSetName = DefaultParamSet)]
+		public SwitchParameter KeepSecretWrapper
+		{ get; set; }
+
+		[Parameter(Mandatory = false, ParameterSetName = SaveAsParamSet)]
+		public string SaveAsProfile
+		{ get; set; }
+
+		[Parameter(Mandatory = false, ParameterSetName = SaveAsParamSet)]
+		public SwitchParameter Force
+		{ get; set; }
+
+		protected override void BeginProcessing()
+		{
+			ResolveVaultClient();
+		}
+
+		protected override void ProcessRecord()
+		{
+			var authMount = MountName ?? AuthMethod;
+			var resPath = $"auth/{authMount}/login";
+			if (!string.IsNullOrEmpty(PathData))
+				resPath += $"/{PathData.TrimStart('/')}";
+
+			var r = _session.MakeVaultApiRequest<Secret<Dictionary<string, object>>>(
+					resPath, HttpMethod.Post, AuthData).Result;
+
+			if (!string.IsNullOrEmpty(SaveAsProfile))
+			{
+				var token = r.AuthorizationInfo.ClientToken;
+				if (string.IsNullOrEmpty(token))
+					throw new Exception("empty token");
+
+				SetProfile.SetVaultProfile(this, SaveAsProfile, force: Force,
+						vaultAddress: _session.VaultAddress, vaultToken: token);
+			}
+			else
+			{
+				if (KeepSecretWrapper.IsPresent)
+					WriteObject(r);
+				else
+					WriteObject(r.AuthorizationInfo);
+			}
+		}
+	}
+}
