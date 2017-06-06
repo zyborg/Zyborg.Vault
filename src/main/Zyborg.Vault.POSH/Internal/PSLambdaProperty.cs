caa09433d2918f2bc15f212381620b47e296660a
Eugene Bekker <ebekker@powervision.com>
2017-06-06 15:14:10 -0400

    Adding lambda-implemented Property and Method handlers

---
+++ src/main/Zyborg.Vault.POSH/Internal/PSLambdaProperty.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+
+namespace Zyborg.Vault.POSH.Internal
+{
+	public class PSLambdaProperty<T> : PSPropertyInfo
+	{
+		private Func<T> _getter;
+		private Action<T> _setter;
+
+		public PSLambdaProperty(string name, Func<T> getter, Action<T> setter = null)
+		{
+			SetMemberName(name);
+			_getter = getter;
+			_setter = setter;
+		}
+
+		public override PSMemberTypes MemberType => PSMemberTypes.CodeProperty;
+
+		public override string TypeNameOfValue => typeof(T).FullName;
+
+		public override bool IsGettable => _getter != null;
+
+		public override bool IsSettable => _setter != null;
+
+		public override object Value
+		{
+			get => _getter();
+			set => _setter((T)value);
+		}
+
+		public override PSMemberInfo Copy()
+		{
+			return new PSLambdaProperty<T>(Name, _getter, _setter);
+		}
+	}
+
+	public class PSLambdaProperty<T, TArg> : PSPropertyInfo
+	{
+		private TArg _arg;
+		private Func<TArg, T> _getter;
+		private Action<TArg, T> _setter;
+
+		public PSLambdaProperty(String name, TArg arg, Func<TArg, T> getter, Action<TArg, T> setter = null)
+		{
+			SetMemberName(name);
+			_arg = arg;
+			_getter = getter;
+			_setter = setter;
+		}
+
+		public override PSMemberTypes MemberType => PSMemberTypes.CodeProperty;
+
+		public override string TypeNameOfValue => typeof(T).FullName;
+
+		public override bool IsGettable => _getter != null;
+
+		public override bool IsSettable => _setter != null;
+
+		public override object Value
+		{
+			get => _getter(_arg);
+			set => _setter(_arg, (T)value);
+		}
+
+		public override PSMemberInfo Copy()
+		{
+			return new PSLambdaProperty<T, TArg>(Name, _arg, _getter, _setter);
+		}
+	}
+
+	public class PSLambdaMethod : PSMethodInfo
+	{
+		private IEnumerable<Type> _argTypes;
+		private Action<object[]> _handler;
+
+		public PSLambdaMethod(string name, IEnumerable<Type> argTypes, Action<object[]> handler)
+		{
+			SetMemberName(name);
+			_argTypes = argTypes;
+			_handler = handler;
+		}
+
+		public override Collection<string> OverloadDefinitions
+		{
+			get
+			{
+				return new Collection<string>
+				{
+					PSLambdaMethod.ComputeDefinition(Name, null, _argTypes)
+				};
+			}
+		}
+
+		public override PSMemberTypes MemberType => PSMemberTypes.CodeMethod;
+
+		// Consistent with PSCodeMethod implementation
+		public override string TypeNameOfValue => typeof(PSCodeMethod).FullName;
+
+		public override PSMemberInfo Copy()
+		{
+			return new PSLambdaMethod(Name, _argTypes, _handler);
+		}
+
+		public override object Invoke(params object[] arguments)
+		{
+			_handler(arguments);
+			return null;
+		}
+
+		internal static string ComputeDefinition(string name, Type retType, IEnumerable<Type> argTypes)
+		{
+			var builder = new StringBuilder();
+			if (retType != null)
+				builder.Append(retType.FullName).Append(" ");
+			builder.Append($"{name}(");
+			if (argTypes != null)
+				builder.Append(string.Join(", ", argTypes.Select(x => x.FullName)));
+			builder.Append(")");
+			return builder.ToString();
+		}
+	}
+
+	public class PSLambdaMethod<TResult> : PSMethodInfo
+	{
+		private IEnumerable<Type> _argTypes;
+		private Func<object[], TResult> _handler;
+
+		public PSLambdaMethod(string name, IEnumerable<Type> argTypes, Func<object[], TResult> handler)
+		{
+			SetMemberName(name);
+			_argTypes = argTypes;
+			_handler = handler;
+		}
+
+		public override Collection<string> OverloadDefinitions
+		{
+			get
+			{
+				return new Collection<string>
+				{
+					PSLambdaMethod.ComputeDefinition(Name, typeof(TResult), _argTypes)
+				};
+			}
+		}
+
+		public override PSMemberTypes MemberType => PSMemberTypes.CodeMethod;
+
+		// Consistent with PSCodeMethod implementation
+		public override string TypeNameOfValue => typeof(PSCodeMethod).FullName;
+
+		public override PSMemberInfo Copy()
+		{
+			return new PSLambdaMethod<TResult>(Name, _argTypes, _handler);
+		}
+
+		public override object Invoke(params object[] arguments)
+		{
+			return _handler(arguments);
+		}
+	}
+}
