bfbd7477e34d7164b3c1e49f1919ba6541995284
Eugene Bekker <ebekker@powervision.com>
2017-06-06 15:13:20 -0400

    Adding way to test for Initialized status

---
+++ src/main/Zyborg.Vault.POSH/TestInstance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+using VaultSharp.Backends.System.Models;
+
+namespace Zyborg.Vault.POSH
+{
+	[Cmdlet(VerbsDiagnostic.Test, "Instance")]
+	public class TestInstance : VaultBaseCmdlet
+	{
+		protected override void BeginProcessing()
+		{
+			ResolveVaultClient();
+		}
+
+		protected override void ProcessRecord()
+		{
+			base.WriteAsyncResult(_client.GetInitializationStatusAsync());
+		}
+	}
+}
