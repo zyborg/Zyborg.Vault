5cdcf85fc0a28bbc84546d8858c10b21f85818c1
Eugene Bekker <ebekker@powervision.com>
2017-06-06 10:31:20 -0400

    Adding policy management

---
+++ src/main/Zyborg.Vault.POSH/RemovePolicy.cs
@@ -0,0 +1,25 @@
+﻿using System.Management.Automation;
+
+namespace Zyborg.Vault.POSH
+{
+	[Cmdlet(VerbsCommon.Remove, "Policy")]
+	public class RemovePolicy : VaultBaseCmdlet
+	{
+		[Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true)]
+		public string[] Name
+		{ get; set; }
+
+		protected override void BeginProcessing()
+		{
+			ResolveVaultClient();
+		}
+
+		protected override void ProcessRecord()
+		{
+			foreach (var n in Name)
+			{
+				AsyncWait(_client.DeletePolicyAsync(n));
+			}
+		}
+	}
+}
---
+++ src/main/Zyborg.Vault.POSH/SetPolicy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Management.Automation;
+using VaultSharp.Backends.System.Models;
+
+namespace Zyborg.Vault.POSH
+{
+	[Cmdlet(VerbsCommon.Set, "Policy")]
+	[OutputType(typeof(Secret<Dictionary<string, object>>))]
+	public class SetPolicy : VaultBaseCmdlet
+	{
+		// Single-Path, Multiple Key/Value sets
+		[Parameter(Mandatory = true, Position = 0,
+				ValueFromPipelineByPropertyName = true)]
+		public string Name
+		{ get; set; }
+
+		[Parameter(Mandatory = true, Position = 1,
+				ValueFromPipelineByPropertyName = true)]
+		public string Rules
+		{ get; set; }
+
+		protected override void BeginProcessing()
+		{
+			base.ResolveVaultClient();
+		}
+
+		protected override void ProcessRecord()
+		{
+			var p = new Policy
+			{
+				Name = Name,
+				Rules = Rules,
+			};
+			AsyncWait(_client.WritePolicyAsync(p));
+		}
+	}
+}
