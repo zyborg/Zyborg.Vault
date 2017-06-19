35a9c1cd11a95911f4024a505ae788814a955a3a
Eugene Bekker <ebekker@powervision.com>
2017-06-19 11:16:45 -0400

    Adding support to build HTML API docs during CI

---
+++ tools/ci/DTG/DocTreeGenerator-config.psd1
@@ -0,0 +1,48 @@
+﻿# Configuration file for Publish-ModuleDocumentationTree
+
+## NOTE:  THIS IS NOT CURRENTLY IN USE!!!
+##        MAY BE DELETED EVENTUALLY IF WE DON'T SWITCH TO IT
+
+@{
+
+# Location of your source control repository containing your modules
+ProjectRoot = '.'
+
+# List each module here with name and paths (relative to project root)
+Modules = @(
+	@{
+		Name = 'Zyborg.Vault'
+		SourcePath = '..\Debug'
+		BinPath = '.'
+	}
+	#,
+	#@{
+	#	Name = 'Acme.Glom.Admin.PowerShell'
+	#	SourcePath = 'projects\Glom.Tools\src\Acme.Glom.Admin.PowerShell'
+	#	BinPath = 'projects\Glom.Tools\output\Debug'
+	#}
+)
+
+# The parent directory name of all your modules
+Namespace = 'Zyborg.Vault'
+
+# Title on each web page
+# (note that the Namespace is prefixed to this title)
+DocTitle = ' - Zyborg Vault PowerShell API'
+
+# Output directory (relative to your current directory)
+DocPath = '.\api-docs'
+
+# HTML template file for DocTreeGenerator (relative to project root)
+#TemplatePath = 'projects\Common\doc\psdoc_template.html'
+
+# Location of namespace_overview.html file (relative to project root)
+#NamespaceOverviewPath = 'projects\Common\doc'
+
+# Information added to web pages
+CopyrightYear = 'Copyright (C) Zyborg.'
+
+# Information added to web pages
+RevisionDate = '2017.06.06'
+
+}
---
+++ tools/ci/on_succsess-DocTreeGen.ps1
@@ -0,0 +1,67 @@
+
+## This script generates HTML API Documentation for Zyborg.Vault module
+
+## Install and load the DTG module
+Install-Module DocTreeGenerator -ErrorAction Stop
+Import-Module DocTreeGenerator -Force
+
+## We need to "FIX" the DTG module to handle modules with a DefaultCommandPrefix
+## defined, until [this](https://github.com/msorens/DocTreeGenerator/pull/8) is
+## merged in and the tool is republished.
+$dtg = Get-Module DocTreeGenerator
+$dtgBase = $dtg.ModuleBase
+$dtgFile = "$dtgBase\Source\Convert-HelpToHtmlTree.ps1"
+$dtgLines = [System.IO.File]::ReadAllLines($dtgFile)
+Rename-Item  -Path $dtgFile -NewName "$($dtgFile).ORIG"
+$dtgLines[434] = $dtgLines[434].Replace('$_', '$function')
+[System.IO.File]::WriteAllLines($dtgFile, $dtgLines)
+
+
+## Resolve input parameters and paths
+$projectRoot = "." ## Root of Zyborg.Vault
+$ciDir = "$projectRoot\tools\ci"
+
+$templatePath = "$ciDir\DTG\bootstrap4_template.html"
+
+$workDir = "$projectRoot\work"
+mkdir $workDir -Force
+
+$sourceDir = "$workDir\ZyborgSource"
+$targetDir = "$workDir\api-docs"
+
+mkdir $sourceDir -Force
+mkdir $targetDir -Force
+
+$ns    = "Zyborg"
+$nsDir = "$workDir\ZyborgSource\$ns"
+
+mkdir $nsDir -Force
+
+## Copy over the latest build output to a folder structure required by DTG
+##    SourceDir\NamespaceName\ModuleName\*
+$buildConfig = $env:CONFIGURATION
+if (-not $buildConfig) {
+    $buildConfig = "Debug"
+}
+$buildOutDir = "$projectRoot\src\main\Zyborg.Vault.POSH\bin\$buildConfig\Zyborg.Vault"
+
+Copy-Item -Path $buildOutDir -Destination $nsDir -Recurse
+Copy-Item -Path "$ciDir\DTG\namespace_overview.html" -Destination $nsDir
+Copy-Item -Path "$ciDir\DTG\module_overview.html" -Destination "$nsDir\Zyborg.Vault"
+
+$docTitle  = "Vault API"
+$copyright = "Copyright (C) Eugene Bekker.  All rights reserved."
+$revDate   = (Get-Date -Format 'yyyy-MM-dd HH:mm:ss')
+if ($env:BUILD_NUMBER) {
+    $revDate += " ($($env:BUILD_NUMBER))"
+}
+
+Convert-HelpToHtmlTree `
+        -Namespaces   $ns `
+        -SourceDir    $sourceDir `
+        -TargetDir    $targetDir `
+        -TemplateName $templatePath `
+        -DocTitle     $docTitle `
+        -Copyright    $copyright `
+        -RevisionDate $revDate `
+        -Verbose
