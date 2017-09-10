398b99489eb95221c3873a704fb8fbd0fc8244ed
Eugene Bekker <ebekker@powervision.com>
2017-09-10 07:29:48 -0400

    Make it easily reusable for calling from the CLI

--- client/test/Zyborg.Vault-tests/TestConfig.cs
+++ client/test/Zyborg.Vault-tests/TestConfig.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Zyborg.Vault
 {
@@ -22,17 +23,17 @@ namespace Zyborg.Vault
                     [FileConfigVaultAddress] =
                             new[]
                             {
-                                "efd737871f6fcd1464b71ce185d7c5cc6b89ef75e4e1b42cd01ce6996e06db16a1",
-                                "aa4efa7677eeae314e77b4c3044bd5b99cb2a231d7cbc9621f3052928e2af33195",
-                                "bbb8d156a1b80c96ad001540528115f3949dd32c9828e3072cfb63e662e1088efe",
-                            },
+                                "vault unseal efd737871f6fcd1464b71ce185d7c5cc6b89ef75e4e1b42cd01ce6996e06db16a1",
+                                "vault unseal aa4efa7677eeae314e77b4c3044bd5b99cb2a231d7cbc9621f3052928e2af33195",
+                                "vault unseal bbb8d156a1b80c96ad001540528115f3949dd32c9828e3072cfb63e662e1088efe",
+                            }.Select(x => x.Replace("vault unseal ","")),
                     [MockServerVaultAddress] =
                             new[]
                             {
-                                "6111CC357474407411E86AE9B5575603EAD6C12889B2692E35A07D322E75366A01",
-                                "55F23CD6AD457CB688F1FBBCE68752E78D7F12A3FA360607E4459D31C9E64F1102",
-                                "B05A6C7E13A36801FF0F7D8FD73EA7BB5918AADA224A23E9ABEF34309497913803"
-                            },
+                                "vault unseal 6111CC357474407411E86AE9B5575603EAD6C12889B2692E35A07D322E75366A01",
+                                "vault unseal 55F23CD6AD457CB688F1FBBCE68752E78D7F12A3FA360607E4459D31C9E64F1102",
+                                "vault unseal B05A6C7E13A36801FF0F7D8FD73EA7BB5918AADA224A23E9ABEF34309497913803"
+                            }.Select(x => x.Replace("vault unseal ","")),
                 };
 
         public static readonly IReadOnlyDictionary<string, string> RootTokens =
